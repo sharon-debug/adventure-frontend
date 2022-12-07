b03c1ccf9eb79a2226d4f7ad752549ae63d2d659
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-12-07 20:52:23 +0300

    project

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+    return
+    
+     <h1> Lets Travel the word</h1>
+
+    
+}
+
+export default Home;
---
+++ src/components/data.js
@@ -0,0 +1,53 @@
+
+import React from 'react';
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from 'react-icons/fa';
+export const links = [
+  {
+    id: 1,
+    url: '/',
+    text: 'Home',
+  },
+  {
+    id: 2,
+    url: '/ Houses',
+    text: 'Houses',
+  },
+  {
+    id: 3,
+    url: '/Reviews',
+    text: 'Reviews',
+  },
+  {
+    id: 4,
+    url: '/Profile',
+    text: 'Profile',
+  },
+  {
+    id: 5,
+    url: '/Login',
+    text: 'Login',
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: 'https://www.twitter.com',
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: 'https://www.twitter.com',
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: 'https://www.twitter.com',
+    icon: <FaLinkedin />,
+  },
+  {
+    id: 4,
+    url: 'https://www.twitter.com',
+    icon: <FaBehance />,
+  },
+];
